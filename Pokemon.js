e80fc165d23a69e94fcb18e85d9ccad1ab72d082
janwillem2020 <70693700+janwillem2020@users.noreply.github.com>
2022-04-22 09:57:41 +0200

    3 functies gemaakt

--- Pokemon.js
+++ Pokemon.js
@@ -10,4 +10,24 @@ class Pokemon {
         this.weakness = weakness;
         Pokemon.amountOfPokemon++;
     }
+
+    attack(target, attack) {
+        target.takeDamage(this.energyType, this.attacks[attack])
+    }
+
+    takeDamage(energyType, attack) {
+        if (energyType.name == this.resistance.energyType.name) {
+            attack.value = attack.value - this.resistance.value;
+        }
+
+        if (energyType.name == this.weakness.energyType.name) {
+            this.health = this.health - attack.value * this.weakness.value;
+        } else {
+            this.health = this.health - attack.value
+        }
+    }
+
+    getAmountOfPokemon() {
+        return amountOfPokemon;
+    }
 }
